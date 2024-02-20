e2b7011ca41b7558a62b6b69874cea34cf784225
Ahmadjajja <ahmadjajja86@gmail.com>
2024-02-20 18:33:04 +0500

    front-end setup

---
+++ client/src/Pages/LandingPage/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../Components/Header";
+import LandingPage from "./landingPage";
+function index() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <p>Body...</p>
+      </main>
+    </div>
+  );
+}
+
+export default index;
