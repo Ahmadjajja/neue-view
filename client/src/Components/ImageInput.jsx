64e0fabb67be599341246a29b1c8bb999b9ca185
Ahmadjajja <ahmadjajja86@gmail.com>
2024-02-22 00:26:16 +0500

    fixes

--- client/src/Components/ImageInput.jsx
+++ client/src/Components/ImageInput.jsx
@@ -1,34 +1,34 @@
 import React from 'react'
-import vector from "../assets/Vector.png"
+import vector from "../Assets/Vector.png"
 import PrimaryBtn from './PrimaryBtn'
 
 const ImageInput = () => {
   return (
     <div>
-      
-      <div class="uploadOuter" style={{height:"25rem"}} >
-      <span class="dragBox " >
-      <div className="row mt-5">
-      <div className='col-12'><img src={vector} alt="" />
+
+      <div class="uploadOuter" style={{ height: "25rem" }} >
+        <span class="dragBox " >
+          <div className="row mt-5">
+            <div className='col-12'><img src={vector} alt="" />
+            </div>
+          </div>
+          <div className="row">
+            <div className='col-12'>
+              Drag and drop image or upload from your file.(jpg, png, pdf)</div>
+          </div>
+          <span><b> Or</b></span>
+          <div className="row  d-flex ">
+
+            {/*    <PrimaryBtn width={"6rem"} text={"Camera"} />*/}
+            <PrimaryBtn width={"6rem"} text={"Upload"} />
+
+          </div>
+          <input type="file" onChange="dragNdrop(event)" ondragover="drag()" ondrop="drop()" id="uploadFile" />
+        </span>
       </div>
-      </div>
-      <div className="row">
-      <div className='col-12'>
-      Drag and drop image or upload from your file.(jpg, png, pdf)</div>
-      </div>
-      <span><b> Or</b></span>
-      <div className="row  d-flex ">
-     
-    {/*    <PrimaryBtn width={"6rem"} text={"Camera"} />*/} 
-      <PrimaryBtn width={"6rem"} text={"Upload"}/>
-     
-      </div>
-    <input type="file" onChange="dragNdrop(event)"  ondragover="drag()" ondrop="drop()" id="uploadFile"  />
-</span>
-</div>
 
 
-     
+
     </div>
   )
 }
